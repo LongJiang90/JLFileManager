0037b032462a015aea012adc3402b421d46abdda
jianglong <983220205@qq.com>
2018-03-13 16:13:34 +0800

    完成第一版本开发，完善功能

---
+++ JLFileManager/Classes/ShowFileVC.h
@@ -0,0 +1,12 @@
+//
+//  ShowFileVC.h
+//  AFNetworking
+//
+//  Created by xp on 2018/3/13.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowFileVC : UIViewController
+
+@end
---
+++ JLFileManager/Classes/ShowFileVC.m
@@ -0,0 +1,36 @@
+//
+//  ShowFileVC.m
+//  AFNetworking
+//
+//  Created by xp on 2018/3/13.
+//
+
+#import "ShowFileVC.h"
+
+@interface ShowFileVC ()
+
+@end
+
+@implementation ShowFileVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
